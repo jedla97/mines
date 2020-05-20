b7cdcb08a0e712fb72817c2977983187c0ef534e
Jakub Jedlička <jakub.jedla.jedlicka@gmail.com>
2020-05-21 00:02:26 +0200

    Add final gamemechanic + icons
    add icons for bomb, flag and flaged bomb
    add oprtion for flag cell
    add option for new game
    add counter of used flags  with no limit
    add 2 atributs to cell
    to rules:
    add winner method
    Add multiple methods for refreshand modifide cells and game field
    edit some gamemechanic to function properly

--- MainApp.java
+++ MainApp.java
@@ -11,6 +11,8 @@ import javax.swing.JTextField;
 
 public class MainApp {
 
+	private int counter = 0;
+	private int clicCounter = 0;
 	private JFrame frame;
 	private JTextField flagCounter;
 
@@ -52,45 +54,84 @@ public class MainApp {
 		frame.setBounds(100, 100, 514, 579);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+
+		JTextField winner = new JTextField();
+		winner.setBounds(100, 190, 300, 150);
+		frame.getContentPane().add(winner);
+		winner.setColumns(15);
+		winner.setEditable(false);
+		winner.setText("WINNER");
+		winner.setHorizontalAlignment(JTextField.CENTER);
+		winner.setBorder(null);
+		winner.setVisible(false);
 		
 		// create dynamic field of buttons
 		// for now is set to 10x10 field
-		for(int i = 40; i <= 490 ;i = i + 50) {
+		for (int i = 40; i <= 490; i = i + 50) {
 			ArrayList<Cell> cel = new ArrayList<Cell>();
-			for(int j = 0; j <= 450 ;j = j + 50) {
+			for (int j = 0; j <= 450; j = j + 50) {
 				JButton button = new JButton("");
 				button.setBounds(j, i, 50, 50);
 				button.addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						r.onClicAction(cels, button);
+						if (clicCounter % 2 == 0) {
+							r.onClicAction(cels, button);
+						} else {
+							r.setFlag(cels, button);
+							r.setCounter(flagCounter, r.numberOfFlags(cels));
+						}
+						if(r.isWinner(cels)== true) {
+							winner.setVisible(true);
+						}
 					}
-					
+
 				});
 				frame.getContentPane().add(button);
 				Cell c = new Cell(button);
-				cel.add(c);				
+				cel.add(c);
 			}
 			cels.add(cel);
 		}
+
 		
-		JButton newGame = new JButton("NewGame");
-		newGame.setBounds(205, 0, 85, 40);
-		frame.getContentPane().add(newGame);
-		
+		flagCounter = new JTextField();
+		flagCounter.setBounds(105, 1, 85, 38);
+		frame.getContentPane().add(flagCounter);
+		flagCounter.setColumns(10);
+		flagCounter.setEditable(false);
+		flagCounter.setText("Used flags = 0");
+		flagCounter.setHorizontalAlignment(JTextField.CENTER);
+		flagCounter.setBorder(null);
+
 		JButton flag = new JButton("Flag");
 		flag.setBounds(305, 0, 85, 40);
 		frame.getContentPane().add(flag);
-		
-		flagCounter = new JTextField();
-		flagCounter.setBounds(105, 1, 85, 40);
-		frame.getContentPane().add(flagCounter);
-		flagCounter.setColumns(10);
-		
-		JButton flag_1 = new JButton("Flag");
-		flag_1.setBounds(405, 0, 85, 40);
-		frame.getContentPane().add(flag_1);
+
+		flag.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				clicCounter++;
+			}
+
+		});
+
+		JButton newGame = new JButton("NewGame");
+		newGame.setBounds(205, 0, 85, 40);
+		frame.getContentPane().add(newGame);
+
+		newGame.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				r.initCell(cels);
+				counter = 0;
+				clicCounter = 0;
+				r.setCounter(flagCounter, counter);
+			}
+
+		});
+
 		return cels;
 	}
 }
